f05f5501c3dcbb7898281531b4e10929779186ad
Alex Ranc <ranc@deepsleepstudio.com>
2014-11-12 17:01:14 -0500

    removed "reduced" class from nav
    when we relased website I had to center the navigation but now that we have more pages on the menu we have to float it next to the logo

--- wp-content/themes/miamibeach100/nav.php
+++ wp-content/themes/miamibeach100/nav.php
@@ -1,7 +1,7 @@
 		<header>
 			<div class="container">
 				<a href="<?php echo home_url(); ?>"><img class="mb-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="miami beach logo"></a>
-				<nav class="reduced">
+				<nav>
 					<li class="mobile-nav"><a href=""><i class="fa fa-bars"></i> Navigation</a></li>
 					<?php wp_nav_menu(); ?>
 				</nav>
