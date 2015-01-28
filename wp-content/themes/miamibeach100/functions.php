b7298a9bed472b4612e497a0ffd8997162fe8d06
Alex Ranc <alexranc@Riveras-iMac.local>
2015-01-28 16:49:26 -0500

    create pics CPT in functions

--- wp-content/themes/miamibeach100/functions.php
+++ wp-content/themes/miamibeach100/functions.php
@@ -70,9 +70,9 @@ add_action( 'init', 'custom_post_type');
 function custom_post_type() {
 
 	$labels = array(
-		'name'                => _x( 'Offers', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Offer', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Offers', 'text_domain' ),
+		'name'                => _x( 'Pics', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Pics', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Pics', 'text_domain' ),
 		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
 		'all_items'           => __( 'All Items', 'text_domain' ),
 		'view_item'           => __( 'View Item', 'text_domain' ),
@@ -85,25 +85,25 @@ function custom_post_type() {
 		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
 	);
 	$args = array(
-		'label'               => __( 'offers', 'text_domain' ),
-		'description'         => __( 'CPT for Offers page', 'text_domain' ),
+		'label'               => __( 'pics', 'text_domain' ),
+		'description'         => __( 'CPT for pics page', 'text_domain' ),
 		'labels'              => $labels,
-		'supports'            => array( 'title', ),
-		'hierarchical'        => true,
+		'supports'            => array( 'title', 'custom-fields', ),
+		'hierarchical'        => false,
 		'public'              => true,
 		'show_ui'             => true,
 		'show_in_menu'        => true,
-		'show_in_nav_menus'   => false,
-		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
 		'menu_position'       => 5,
-		'menu_icon'           => 'dashicons-megaphone',
+		'menu_icon'           => 'dashicons-format-gallery',
 		'can_export'          => true,
 		'has_archive'         => true,
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'capability_type'     => 'page',
 	);
-	register_post_type( 'offers', $args );
+	register_post_type( 'pics', $args );
 
 }
 
